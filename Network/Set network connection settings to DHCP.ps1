4695e250229288e2551ba928684373250287af90
Christian Jäckle <info@christianjaeckle.de>
2017-03-06 13:53:55 +0100

    Change only one named adapter

--- Network/Set network connection settings to DHCP.ps1
+++ Network/Set network connection settings to DHCP.ps1
@@ -1,29 +1,30 @@
 $IPProtocol = "IPv4"
+$AdapterName = "Ethernet"
 
-# Get only active adapters
-Write-Verbose "Get only active adapters."
-$ActiveAdapters = Get-NetAdapter | ? { $_.Status -eq "Up" }
+# Get only active adapter with name "Ethernet"
+Write-Verbose "Get only active adapter with name '$AdapterName'."
+$ActiveAdapter = Get-NetAdapter -Name $AdapterName | ? { $_.Status -eq "Up" }
 
 # Get IPv4 interfaces
-Write-Verbose "Get IPv4 interfaces."
-$IP4Interfaces = $ActiveAdapters | Get-NetIPInterface -AddressFamily $IPProtocol
+Write-Verbose "Get IPv4 interface."
+$IP4Interface = $ActiveAdapter | Get-NetIPInterface -AddressFamily $IPProtocol
 
-If ($IP4Interfaces.Dhcp -eq "Disabled") {
+If ($IP4Interface.Dhcp -eq "Disabled") {
     Write-Verbose "DHCP is currently disabled."
 
     # Remove existing standard gateway
-    If (($IP4Interfaces | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+    If (($IP4Interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
         Write-Verbose "Remove existing standard gateway."
-        $IP4Interfaces | Remove-NetRoute -Confirm:$false
+        $IP4Interface | Remove-NetRoute -Confirm:$false
     }
     
     # Enable DHCP
     Write-Verbose "Enable DHCP."
-    $IP4Interfaces | Set-NetIPInterface -DHCP Enabled
+    $IP4Interface | Set-NetIPInterface -DHCP Enabled
     
     # Set DNS Servers to automatic
     Write-Verbose "Set DNS Servers to automatic"
-    $IP4Interfaces | Set-DnsClientServerAddress -ResetServerAddresses
+    $IP4Interface | Set-DnsClientServerAddress -ResetServerAddresses
 }
 else {
     Write-Verbose "DHCP is currently enabled."
