97771148673d3fb6e6a68adc69b024697d4e21d8
Christian Jäckle <info@christianjaeckle.de>
2017-03-09 17:11:49 +0100

    Create Windows 10 - Benachrichtigungen des Synchronisierungsanbieters deaktivieren.ps1

---
+++ Operating System/Windows 10 - Benachrichtigungen des Synchronisierungsanbieters deaktivieren.ps1
@@ -0,0 +1,22 @@
+$Key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$Name = "ShowSyncProviderNotifications"
+$Type = "DWord"
+$Value = "0"
+
+# Check if property already exist
+if((Get-ItemProperty -Path $Key -Name $Name -ErrorAction SilentlyContinue).$Name) {
+	# Get old value
+	$ValueOld = Get-ItemProperty -Path $Key -Name $Name
+	Write-Verbose -Message "Property already exists: $ValueOld"
+    
+    # Remove existing value
+    Remove-ItemProperty -Path $Key -Name $Name
+    Write-Verbose -Message "Property removed."
+}
+else
+{
+    Write-Verbose -Message "Property doesn't exist."
+}
+
+# Add property
+New-ItemProperty -Path $Key -Name $Name -PropertyType $Type -Value $Value -Force
