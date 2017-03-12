5be0877e9673775e1512a3ae698d77c7b0999793
Christian Jäckle <info@christianjaeckle.de>
2017-03-12 14:32:44 +0100

    Update List available timezones.ps1

--- Date-Time/List available timezones.ps1
+++ Date-Time/List available timezones.ps1
@@ -1,2 +1,5 @@
 # List available timezones.
 Get-TimeZone -ListAvailable
+
+# or
+#[System.TimeZoneInfo]::GetSystemTimeZones() | Select-Object Id, DisplayName, BaseUtcOffset, SupportsDaylightSavingTime
