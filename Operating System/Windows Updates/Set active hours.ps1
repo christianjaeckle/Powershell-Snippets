95328d83fea0acda4f4e3785aa2b1a28401e1ba3
Christian Jäckle <info@christianjaeckle.de>
2017-03-20 11:21:53 +0100

    Update Set active hours.ps1

--- Operating System/Windows Updates/Set active hours.ps1
+++ Operating System/Windows Updates/Set active hours.ps1
@@ -1,21 +1,20 @@
 # Set active hours from Windows Update settings
 
 $Key = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings"
+$Type = "DWord"
+$From = "8"
+$To = "18"
 $Name1 = "ActiveHoursStart"
 $Name2 = "ActiveHoursEnd"
-$Type1 = "DWord"
-$Type2 = "DWord"
-$Value1 = "8"
-$Value2 = "18"
 
 # Remove key / name / value if exists
 Remove-ItemProperty -Path $Key -Name $Name1 -ErrorAction SilentlyContinue
 
 # Add key / name / value
-New-ItemProperty -Path $Key -Name $Name1 -PropertyType $Type1 -Value $Value1 -Force
+New-ItemProperty -Path $Key -Name $Name1 -PropertyType $Type -Value $From -Force
 
 # Remove key / name / value if exists
 Remove-ItemProperty -Path $Key -Name $Name2 -ErrorAction SilentlyContinue
 
 # Add key / name / value
-New-ItemProperty -Path $Key -Name $Name2 -PropertyType $Type2 -Value $Value2 -Force
+New-ItemProperty -Path $Key -Name $Name2 -PropertyType $Type -Value $To -Force
