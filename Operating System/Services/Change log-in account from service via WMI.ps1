b9a65499f929c3c8b8a2fd595c3047466b49259f
Christian Jäckle <info@christianjaeckle.de>
2017-03-20 09:30:28 +0100

    Create Change log-in account from service via WMI.ps1

---
+++ Operating System/Services/Change log-in account from service via WMI.ps1
@@ -0,0 +1,14 @@
+
+#Requires -RunAsAdministrator
+
+# Define variables
+$Computer = "."
+$ServiceName = "DevQueryBroker"
+$ServiceUser = "brandmaker\christian.jaeckle"
+$ServicePassword = "31JC_BMExit"
+
+# Get service object
+$wmiService = Get-WmiObject -ComputerName $Computer -Query "SELECT * FROM Win32_Service WHERE Name = '$ServiceName'"
+
+# Set service attribute
+$wmiService.Change($null, $null, $null, $null, $null, $null, "$ServiceUser",$ServicePassword)
