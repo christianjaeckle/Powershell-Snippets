5426de6c4a6df34ecf13ac687aad9aabaadd8575
Christian Jäckle <info@christianjaeckle.de>
2017-03-20 09:31:56 +0100

    Update and rename Change log-in account from service via WMI.ps1 to Change service account via WMI.ps1

--- Operating System/Services/Change service account via WMI.ps1
+++ Operating System/Services/Change service account via WMI.ps1
@@ -10,5 +10,8 @@ $ServicePassword = "31JC_BMExit"
 # Get service object
 $wmiService = Get-WmiObject -ComputerName $Computer -Query "SELECT * FROM Win32_Service WHERE Name = '$ServiceName'"
 
+# Stop service
+Stop-Service -Name $ServiceName
+
 # Set service attribute
 $wmiService.Change($null, $null, $null, $null, $null, $null, "$ServiceUser",$ServicePassword)
