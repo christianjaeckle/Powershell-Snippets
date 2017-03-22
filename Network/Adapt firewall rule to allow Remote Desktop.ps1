5daba7dc445b095533e8810daa9903cb77a98a19
Christian Jäckle <info@christianjaeckle.de>
2017-03-22 18:45:37 +0100

    Update Adapt firewall rule to allow Remote Desktop.ps1

--- Network/Adapt firewall rule to allow Remote Desktop.ps1
+++ Network/Adapt firewall rule to allow Remote Desktop.ps1
@@ -1,1 +1,4 @@
 Set-NetFirewallRule -DisplayName "Remotedesktop - Benutzermodus (TCP eingehend)" -Enabled True
+
+# Or if you would like to allow the whole group
+Enable-NetFirewallRule -DisplayGroup "Remotedesktop"
