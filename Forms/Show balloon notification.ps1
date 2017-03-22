bede5f095d9056b689222360138f794b35fba25e
Christian Jäckle <info@christianjaeckle.de>
2017-03-22 22:13:01 +0100

    Create Show balloon notification.ps1

---
+++ Forms/Show balloon notification.ps1
@@ -0,0 +1,21 @@
+# Load Assembly
+[system.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+
+# Create forms object
+$Notifca = New-Object System.Windows.Forms.NotifyIcon
+
+# Get path of current process
+$Path = Get-Process -id $Pid | Select-Object -ExpandProperty Path
+
+# Get icon of current process
+$Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
+
+# Set properties of baloon tip
+$Notifca.Icon = $Icon
+$Notifca.BalloonTipIcon = "Info"
+$Notifca.BalloonTipText = "This is a demo notification"
+$Notifca.BalloonTipTitle = "Title"
+$Notifca.Visible = $true
+
+# Show tip
+$Notifca.ShowBalloonTip(5000)
