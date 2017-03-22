b192fa2462489aca3561fb5d422b13e446ae8415
Christian Jäckle <info@christianjaeckle.de>
2017-03-22 18:40:05 +0100

    Create Adapt firewall rule to allow ping via IPv4.ps1

---
+++ Network/Adapt firewall rule to allow ping via IPv4.ps1
@@ -0,0 +1,5 @@
+# English
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request – ICMPv4-In)" -Enabled True
+
+# German
+Set-NetFirewallRule -DisplayName "Datei- und Druckerfreigabe (Echoanforderung - ICMPv4 eingehend)" -Enabled True
