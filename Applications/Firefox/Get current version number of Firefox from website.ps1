f87519abdc794c000e07cb06029169923d659135
Christian Jäckle <info@christianjaeckle.de>
2017-03-30 21:10:54 +0200

    Update Get current version number of Firefox from website.ps1

--- Applications/Firefox/Get current version number of Firefox from website.ps1
+++ Applications/Firefox/Get current version number of Firefox from website.ps1
@@ -1,1 +1,29 @@
-11
+# Checking if website is available
+if( (Test-NetConnection -ComputerName "mozilla.org" -CommonTCPPort HTTP).TcpTestSucceeded -eq $true ) {
+    Write-Verbose -Message "Website is available."
+
+    # Request website content
+    $Website = Invoke-WebRequest -Uri "https://www.mozilla.org/de/firefox/new/"
+
+    if($Website.StatusCode -eq 200) {
+        Write-Verbose -Message "Got website content."
+
+        $Url = ($Website.Links | Where-Object { ($_.innerHTML -eq "Windows 64-bit") -and ($_.href -like "*?product=firefox-*") } | Select-Object -Property href -Unique).href
+
+        [int]$PosFirstMinus = $Url.IndexOf("-") + 1
+        [int]$PosLastMinus = $Url.LastIndexOf("-")
+                
+        $Url.Substring($PosFirstMinus,($PosLastMinus - $PosFirstMinus))
+
+    }
+    else {
+        Write-Verbose -Message "Got no website content!"
+
+        Exit 1
+    }
+}
+else {
+    Write-Verbose -Message "Website not available!"
+
+    Exit 1
+}
