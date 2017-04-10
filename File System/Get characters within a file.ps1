d07eff51afd18688197e8a95cbcc7595748d6f53
Christian Jäckle <info@christianjaeckle.de>
2017-04-10 14:52:31 +0200

    Update and rename Count characters within a file.ps1 to Get characters within a file.ps1

--- File System/Get characters within a file.ps1
+++ File System/Get characters within a file.ps1
@@ -1,7 +1,7 @@
 # Get file path to script
 $FileName = (Get-Item $PSCommandPath ).FullName
 
-# Read content and count characters
+# Read content and get characters
 $AmountChar = (Get-Content -Path $FileName | Measure-Object -Character).Characters
 
 # Output
