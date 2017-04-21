56b0d26155a4c2957a224aecdd60d07b07e943db
Christian Jäckle <info@christianjaeckle.de>
2017-04-21 12:37:44 +0200

    Update Uninstall OneDrive.ps1

--- Applications/OneDrive/Uninstall OneDrive.ps1
+++ Applications/OneDrive/Uninstall OneDrive.ps1
@@ -31,12 +31,11 @@ ForEach ($Direcotry in $DirectoriesToClean) {
 }
 
 Write-Output "Delete relicts from registry..."
-
 New-PSDrive -Name "HKCR" -PSProvider Registry -Root "HKEY_CLASSES_ROOT" -ErrorAction SilentlyContinue | Out-Null
-
+Set-ItemProperty -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+Set-ItemProperty -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
 Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -Force
 Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -Force
-
 Remove-PSDrive -Name "HKCR" -Force | Out-Null
 
 Write-Output "Delete relicts from start menu..."
