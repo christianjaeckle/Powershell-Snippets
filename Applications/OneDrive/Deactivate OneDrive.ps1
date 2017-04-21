45a104277a33583f1afe626d586914864e74861a
Christian Jäckle <info@christianjaeckle.de>
2017-04-21 12:37:12 +0200

    Update Deactivate OneDrive.ps1

--- Applications/OneDrive/Deactivate OneDrive.ps1
+++ Applications/OneDrive/Deactivate OneDrive.ps1
@@ -1,5 +1,14 @@
-$Reg_Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive"
-$Reg_Property = "DisableFileSyncNGSC"
+#Requires -RunAsAdministrator
 
-New-Item -Path $RegKey -Force
-Set-ItemProperty -Path $RegKey -Name $Reg_Property -Value "1"
+Write-Output "Kill process..."
+(Get-Process -Name "OneDrive" -ErrorAction SilentlyContinue) | ForEach-Object { $_.Kill() | Out-Null }
+
+Write-Output "Delete relicts from registry..."
+New-PSDrive -Name "HKCR" -PSProvider Registry -Root "HKEY_CLASSES_ROOT" -ErrorAction SilentlyContinue | Out-Null
+Set-ItemProperty -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+Set-ItemProperty -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+Remove-PSDrive -Name "HKCR" -Force | Out-Null
+
+Write-Output "Deactivate via registry..."
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value "1"
