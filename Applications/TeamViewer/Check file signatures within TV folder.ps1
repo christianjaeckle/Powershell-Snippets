861cffc7055d27fcd3c72962d1b10e6b2ce14375
Christian Jäckle <info@christianjaeckle.de>
2017-07-19 21:29:44 +0200

    Create Check file signatures within TV folder.ps1

---
+++ Applications/TeamViewer/Check file signatures within TV folder.ps1
@@ -0,0 +1,12 @@
+$TVFiles = Get-ChildItem -Path "${env:ProgramFiles(x86)}\TeamViewer" -Recurse -Include *.exe,*.dll,*.cat
+
+$TVFiles | Foreach-Object {
+    $CheckResult = Get-AuthenticodeSignature -FilePath $_.FullName
+
+    if ($CheckResult.Status -ne "Valid") {
+        Write-Host $_.FullName -ForegroundColor Yellow
+    }
+    else {
+        Write-Host $_.FullName -ForegroundColor Green
+    }
+}
