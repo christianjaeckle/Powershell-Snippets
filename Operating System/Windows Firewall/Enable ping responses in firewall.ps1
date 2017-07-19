e1f27c55ab48aa89efc842f7b498faaefdc1f154
Christian Jäckle <info@christianjaeckle.de>
2017-07-19 21:28:35 +0200

    Create Enable ping responses in firewall.ps1

---
+++ Operating System/Windows Firewall/Enable ping responses in firewall.ps1
@@ -0,0 +1,6 @@
+# Enable whole group
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request – ICMPv4-In)" -Enabled True
+
+# Enable single rules
+#Set-NetFirewallRule -Name "FPS-ICMP4-ERQ-In" -Enabled True
+#Set-NetFirewallRule -Name "FPS-ICMP4-ERQ-Out" -Enabled True
