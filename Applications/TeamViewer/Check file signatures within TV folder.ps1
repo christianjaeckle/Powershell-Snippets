d296062b2b6fc4a5ab1debc8f42a55f83e551f34
Christian Jäckle <info@christianjaeckle.de>
2017-07-19 21:34:01 +0200

    Update Check file signatures within TV folder.ps1

--- Applications/TeamViewer/Check file signatures within TV folder.ps1
+++ Applications/TeamViewer/Check file signatures within TV folder.ps1
@@ -1,4 +1,4 @@
-$TVFiles = Get-ChildItem -Path "${env:ProgramFiles(x86)}\TeamViewer","${env:ProgramFiles}\TeamViewer" -Recurse -Include *.exe,*.dll,*.cat
+$TVFiles = Get-ChildItem -Path "${env:ProgramFiles(x86)}\TeamViewer","${env:ProgramFiles}\TeamViewer" -Recurse -Include *.exe,*.dll,*.cat -ErrorAction SilentlyContinue
 
 $TVFiles | Foreach-Object {
     $CheckResult = Get-AuthenticodeSignature -FilePath $_.FullName
