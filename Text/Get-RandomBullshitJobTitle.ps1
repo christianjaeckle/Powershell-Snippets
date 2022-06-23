89cb9609f2fe350e2879abfc2120a2b820c11926
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-06-23 18:23:21 +0200

    Add file Get-RandomBullshitJobTitle.ps1

---
+++ Text/Get-RandomBullshitJobTitle.ps1
@@ -0,0 +1,9 @@
+$arrPosi = ("Manager","Director","Student","Trainee","Lead","Chief","Officer","Assistent","Head of","Global","Corporate","Senior","Associate","International","National","Junior","Principal","Networking","Lead","Volunteer","Professional","Independent","Qualitative","First Class","Virtuous","Innovation","President","Mentor","Strategic","Enterprise","Viral","Back-End","Central","Corporate","Holistic","Leading","Next Generation","Official")
+
+$arrDepart = ("Quality","Digital Marketing","Technical Product Management","Support","IT","Operations","Sales","Pre-Sales","Consulting","Customer Success","Marketing","Engineering","Customer Care","Supply Chain","Future","Social Media","Mobile","Functionality")
+
+$arrTitle = ("Analyst","Solution Driver","Manager","Operator","Orchestrator","Technician","Guru","Evangelist","Administrator","Solution Specialist","Specialist","Performer","Organizer","Genius","Trainer","Master","Product Owner","Owner","Optimizer","Strategist","Expert","Designer","Legend","Supporter","Agent","Craftsman","Consultant","Person","Generator","Professor","Programmer","Developer","Designer","Artist","Prophet","Organizer","Innovator","Adopter","Councel","Enthusiast","Coordinator","Advisor","Advocate","Architect","Chairman","Innovator","Planner","Producer")
+
+$arrPosi | Get-Random
+$arrDepart | Get-Random
+$arrTitle | Get-Random
