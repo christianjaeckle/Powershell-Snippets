e7f404bbb81dafdeb34e83300c54af80a42a3a4b
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-07-20 19:32:37 +0200

    Create Unicode literals in PowerShell.ps1

---
+++ Text/Unicode literals in PowerShell.ps1
@@ -0,0 +1,21 @@
+function Unic
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+Unic -Code 255
