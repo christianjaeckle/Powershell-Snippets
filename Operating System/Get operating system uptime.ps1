df464b0451564a3c908e8e74be841334d048603e
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-08-09 21:45:23 +0200

    Update Get operating system uptime.ps1

--- Operating System/Get operating system uptime.ps1
+++ Operating System/Get operating system uptime.ps1
@@ -1,1 +1,10 @@
 (Get-Date) - (Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -ExpandProperty LastBootUpTime)
+
+# OR
+
+function Get-SystemUptime {
+    $OS_Info = Get-WmiObject Win32_OperatingSystem
+    $OS_Info.ConvertToDateTime($OS_Info.LastBootupTime)
+}
+
+Get-SystemUptime
