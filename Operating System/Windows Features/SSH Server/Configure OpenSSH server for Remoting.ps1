7dd3087a8a52ee128952507f5ba8e6a97bf7d92d
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-02-15 12:39:12 +0100

    Create Configure OpenSSH server for Remoting.ps1

---
+++ Operating System/Windows Features/SSH Server/Configure OpenSSH server for Remoting.ps1
@@ -0,0 +1,27 @@
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service automatically
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Start the sshd service
+Start-Service sshd
+
+# Confirm the Firewall rule is configured (automatically created by setup)
+if (!(Get-NetFirewallRule -Name 'PowerShell-Remoting-SSH' -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'PowerShell Remoting via SSH' does not exist, creating it..."
+	
+    New-NetFirewallRule -Name 'PowerShell-Remoting-SSH' -DisplayName 'PowerShell Remoting via SSH' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'PowerShell Remoting via SSH' has been created and exists."
+}
+
+# Set default shell to version 7
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+
+# Add to SSH config ($env:ProgramData\ssh\sshd_config) manually
+#PasswordAuthentication yes
+
+# Connection test
+Test-NetConnection -ComputerName 192.168.0.5
+Test-NetConnection -ComputerName 192.168.0.5 -Port 22
