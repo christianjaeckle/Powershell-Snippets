358d092f157cdb40963e0d5831980dde4095225a
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-02-15 12:52:58 +0100

    Update Deactivate users without login since 6 months.ps1

--- Operating System/Users-Groups/Deactivate users without login since 6 months.ps1
+++ Operating System/Users-Groups/Deactivate users without login since 6 months.ps1
@@ -1,1 +1,3 @@
+#Requires -RunAsAdministrator
+
 Get-LocalUser | Where-Object { ($_.LastLogon -ne $null) -or ($_.LastLogon -ne '') -and ($_.LastLogon -le ((Get-Date).AddDays(-180))) -and ($_.Enabled -eq $true) } | Disable-LocalUser -WhatIf
