da358ca860beb6c038f49101d543cc4b18c0f3f3
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-18 22:34:35 +0100

    Create Set file attribute archive.ps1

---
+++ File System/Attributes/Set file attribute archive.ps1
@@ -0,0 +1,2 @@
+# Set archive attribute
+Set-ItemProperty -Path (Join-Path -Path $env:Userprofile -ChildPath 'Desktop\File.txt') -Name Attributes -Value ((Get-ItemProperty (Join-Path -Path $env:Userprofile -ChildPath 'Desktop\File.txt')).Attributes -bor [System.IO.FileAttributes]::Archive)
