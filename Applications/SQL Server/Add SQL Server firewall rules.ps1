d3a667a16c85d3eba4117afed0a4462b8b0ce56a
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-18 22:14:00 +0100

    Update Add SQL Server firewall rules.ps1

--- Applications/SQL Server/Add SQL Server firewall rules.ps1
+++ Applications/SQL Server/Add SQL Server firewall rules.ps1
@@ -1,3 +1,11 @@
 # Add Windows firewall rules for SQL Server default instance
 New-NetFirewallRule -Direction Inbound -Action Allow -Enabled True -Profile Domain, Private -Protocol TCP -LocalPort 1433 -Group "SQL-Server" -DisplayName "SQL-Server - Default Instance - Inbound" -Description "SQL-Server, Default Instance, Inbound, TCP 1433" -Program "C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Binn\sqlservr.exe"
 New-NetFirewallRule -Direction Inbound -Action Allow -Enabled False -Profile Domain, Private -Protocol UDP -LocalPort 1434 -Group "SQL-Server" -DisplayName "SQL-Server - Default Instance - Browser - Inbound" -Description "SQL-Server, Default Instance, Inbound, Browser, UDP 1434" -Program "C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Binn\sqlservr.exe"
+
+# SQL Analysis Ports
+New-NetFirewallRule -Direction Inbound -Action Allow -Enabled False -Profile Domain, Private -Protocol TCP -LocalPort 2383 -Group "SQL-Server" -DisplayName 'SQL-Server - Analysis Services'
+New-NetFirewallRule -Direction Inbound -Action Allow -Enabled False -Profile Domain, Private -Protocol TCP -LocalPort 2382 -Group "SQL-Server" -DisplayName 'SQL-Server - Browser'
+
+# SQL Special
+New-NetFirewallRule -Direction Inbound -Action Allow -Enabled False -Profile Domain, Private -Protocol TCP -LocalPort 4022 -Group "SQL-Server" -DisplayName 'SQL Service Broker'
+New-NetFirewallRule -Direction Inbound -Action Allow -Enabled False -Profile Domain, Private -Protocol TCP -LocalPort 135 -Group "SQL-Server" -DisplayName 'SQL Debugger/RPC'
