1155fe9c92e2def49ddce1a4c4ce3537bb4c8cc3
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-18 22:36:39 +0100

    Create Set file attribute hidden.ps1

---
+++ File System/Attributes/Set file attribute hidden.ps1
@@ -0,0 +1,2 @@
+# Set hidden attribute
+Set-ItemProperty -Path (Join-Path -Path $env:Userprofile -ChildPath 'Desktop\File.txt') -Name Attributes -Value ((Get-ItemProperty (Join-Path -Path $env:Userprofile -ChildPath 'Desktop\File.txt')).Attributes -bor [System.IO.FileAttributes]::Hidden)
