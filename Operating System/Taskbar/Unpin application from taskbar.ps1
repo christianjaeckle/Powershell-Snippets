16790fab48e742e9ba48338264df3bac0fe5e744
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-18 22:01:38 +0100

    Update Unpin application from taskbar.ps1
    Code formatting

--- Operating System/Taskbar/Unpin application from taskbar.ps1
+++ Operating System/Taskbar/Unpin application from taskbar.ps1
@@ -1,9 +1,10 @@
-if((Get-Culture).Name -eq "de-DE") {
-    $AppName = "Unternehmensportal"
-     
-    ((New-Object -Com Shell.Application).NameSpace("Shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() | ? { $_.Name -eq $AppName } ).Verbs() | ?{ $_.Name.Replace("&", "") -Match "Von Taskleiste lösen" } | %{ $_.DoIt(); $Exec = $true }
-} else {
-    $AppName = "Company Portal"
-     
-    ((New-Object -Com Shell.Application).NameSpace("Shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() | ? { $_.Name -eq $AppName } ).Verbs() | ?{ $_.Name.Replace("&", "") -Match "Unpin from taskbar" } | %{ $_.DoIt(); $Exec = $true }
+if ((Get-Culture).Name -eq 'de-DE') {
+    $AppName = 'Unternehmensportal'
+     
+    ((New-Object -Com Shell.Application).NameSpace('Shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $AppName } ).Verbs() | Where-Object { $_.Name.Replace('&', '') -Match 'Von Taskleiste lösen' } | ForEach-Object { $_.DoIt(); $Exec = $true }
+}
+else {
+    $AppName = 'Company Portal'
+     
+    ((New-Object -Com Shell.Application).NameSpace('Shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $AppName } ).Verbs() | Where-Object { $_.Name.Replace('&', '') -Match 'Unpin from taskbar' } | ForEach-Object { $_.DoIt(); $Exec = $true }
 }
