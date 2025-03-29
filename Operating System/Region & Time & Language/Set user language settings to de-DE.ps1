ca4256a7e98e2e4df705180039310e8365a69600
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-29 21:20:39 +0100

    Create Set user language settings to de-DE.ps1

---
+++ Operating System/Region & Time & Language/Set user language settings to de-DE.ps1
@@ -0,0 +1,2 @@
+# Sets the current user language settings (input method, spelling setting, text prediction, and handwriting input mode, etc.) to German
+Set-WinUserLanguageList -LanguageList 'de-DE' -Confirm:$false -Force
