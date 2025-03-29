c108eee9ab41c925d65eaf5e494b0f4888efbce9
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-29 16:17:29 +0100

    Update Activate Windows Sandbox on Windows 11.ps1

--- Operating System/Windows Features/Windows Sandbox/Activate Windows Sandbox on Windows 11.ps1
+++ Operating System/Windows Features/Windows Sandbox/Activate Windows Sandbox on Windows 11.ps1
@@ -1,4 +1,4 @@
-# Install the Windows Sandbox
+# Enable the Windows Sandbox
 Enable-WindowsOptionalFeature -FeatureName 'Containers-DisposableClientVM' -Online -NoRestart
 
 # A restart of the computer is needed after the installation
