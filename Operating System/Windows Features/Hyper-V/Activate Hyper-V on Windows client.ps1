988a9678482cc097905cd31097a0bdb3dbc1ee17
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-29 20:25:13 +0100

    Update and rename Activate Hyper-V on Windows 11.ps1 to Activate Hyper-V on Windows client.ps1

--- Operating System/Windows Features/Hyper-V/Activate Hyper-V on Windows client.ps1
+++ Operating System/Windows Features/Hyper-V/Activate Hyper-V on Windows client.ps1
@@ -1,4 +1,4 @@
-# Enable Hyper-V
+# Enable optional feature Hyper-V
 Enable-WindowsOptionalFeature -FeatureName 'HypervisorPlatform' -Online -NoRestart
 
 # A restart of the computer is needed after the installation
