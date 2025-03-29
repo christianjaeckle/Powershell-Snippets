84f9f1808d67a803b3003bce5b2cf3ebaabae40c
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-29 15:58:18 +0100

    Update Install Windows Sandbox.ps1

--- Operating System/Windows Features/Windows Sandbox/Install Windows Sandbox.ps1
+++ Operating System/Windows Features/Windows Sandbox/Install Windows Sandbox.ps1
@@ -1,4 +1,4 @@
 # Install the Windows Sandbox
-Enable-WindowsOptionalFeature -FeatureName 'Containers-DisposableClientVM' -All -Online -NoRestart
+Add-WindowsCapability -Name 'Containers-DisposableClientVM' -Online
 
 # A restart of the computer is needed after the installation
