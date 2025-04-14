ff615e62c4400c96afdcff7c55bf84d7d52fc56e
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-14 18:52:01 +0200

    Update Convert current datetime to Unix time stamp.ps1

--- Date-Time/Convert current datetime to Unix time stamp.ps1
+++ Date-Time/Convert current datetime to Unix time stamp.ps1
@@ -1,5 +1,1 @@
-# Variable for Unix time (is fix, don't change it)
-$datUnixStartDate = New-Object DateTime(1970,1,1,0,0,0,([DateTimeKind]::UTC))
- 
-# Calculate difference from now / current time in seconds
-[int]([DateTime]::UtcNow - $datUnixStartDate).TotalSeconds
+Get-Date -UFormat %s -Millisecond 0
