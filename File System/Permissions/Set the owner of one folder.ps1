1b92063c3e621d36fde2bc44f11df82150a85c90
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-14 18:49:23 +0200

    Update Set the owner of one folder.ps1

--- File System/Permissions/Set the owner of one folder.ps1
+++ File System/Permissions/Set the owner of one folder.ps1
@@ -1,9 +1,9 @@
 # Get current ACLs from folder
-$Fol_Acl = Get-Acl 'X:\Folder'
+$Fol_Acl = Get-Acl -Path 'X:\Folder'
 
 # Get user and define as owner
 $Acc_Principle = New-Object System.Security.Principal.Ntaccount("DOMAIN.USERNAME")
 $Fol_Acl.SetOwner($Acc_Principle)
 
 # Set new ACLs for folder
-$Fol_Acl | Set-Acl 'X:\Folder'
+$Fol_Acl | Set-Acl -Path 'X:\Folder'
