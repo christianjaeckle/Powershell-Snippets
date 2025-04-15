f4052a9148a750ee41f054c2f513003410424266
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-15 12:16:14 +0200

    Update Create session with JiraPS.ps1

--- Modules/JiraPS/Create session with JiraPS.ps1
+++ Modules/JiraPS/Create session with JiraPS.ps1
@@ -3,8 +3,10 @@
 # Authentication
 if ((Get-JiraSession) -eq $null) {
     Write-Output "Set credentials for Jira..."
+    
     $JiraCredentials = Get-Credential -Message "Please enter your credentials for Jira." -UserName "$env:username"
 
     Write-Output "Create new Jira session..."
+    
     New-JiraSession -Credential $JiraCredentials
 }
