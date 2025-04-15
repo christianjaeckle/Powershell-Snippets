35126aad3f0d1e1d3dd1a1f62f1b45bedb5c5531
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-15 12:06:34 +0200

    Create List specific installed package.ps1

---
+++ Modules/WinGet/List specific installed package.ps1
@@ -0,0 +1,8 @@
+# List by id, e.g. PowerShell
+Get-WinGetPackage -Id 'Microsoft.PowerShell'
+
+# List by moniker, e.g. PowerShell
+Get-WinGetPackage -Moniker 'pwsh'
+
+# List by name, e.g. PowerShell
+Get-WinGetPackage -Name 'PowerShell'
