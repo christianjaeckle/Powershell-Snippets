ee6d11f5b4b2cfe3d2e3373626cf2160f879a15b
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-15 12:19:55 +0200

    Update Install PSWindowsUpdate.ps1

--- Modules/PSWindowsUpdate/Install PSWindowsUpdate.ps1
+++ Modules/PSWindowsUpdate/Install PSWindowsUpdate.ps1
@@ -1,7 +1,4 @@
 #Requires -RunAsAdministrator
 
-# Add provider for packages
-Install-PackageProvider -Name PSWindowsUpdate -Force
-
 # Install new module
 Install-Module -Name PSWindowsUpdate -Repository PSGallery -Force
