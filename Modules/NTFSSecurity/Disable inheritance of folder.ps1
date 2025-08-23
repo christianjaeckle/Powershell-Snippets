51d85f0992481d55586ae769d1c1bca65564f102
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-08-23 15:10:58 +0200

    Update Disable inheritance of folder.ps1

--- Modules/NTFSSecurity/Disable inheritance of folder.ps1
+++ Modules/NTFSSecurity/Disable inheritance of folder.ps1
@@ -1,2 +1,2 @@
-# Disable inheritance for folder and remove inherited permissions
+# Disable inheritance of folder and remove inherited permissions
 Disable-NTFSAccessInheritance -Path = 'C:\Example' -RemoveInheritedAccessRules = $True
