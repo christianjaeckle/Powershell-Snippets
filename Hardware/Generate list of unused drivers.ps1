6e53591ce859cc67279d3e48b8ff23a055f94ed4
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-09-28 21:46:12 +0200

    Update Generate list of unused drivers.ps1

--- Hardware/Generate list of unused drivers.ps1
+++ Hardware/Generate list of unused drivers.ps1
@@ -1,3 +1,6 @@
 $ActiveDrivers = (Get-PnpDeviceProperty -InputObject (Get-PnpDevice -PresentOnly) -KeyName DEVPKEY_Device_DriverInfPath | Select-Object -Property Data -Unique).Data
 $StoredDrivers = Get-WindowsDriver -Online
 $DriversToRemove = $StoredDrivers | Where-Object -Property Driver -NotIn $ActiveDrivers
+
+# Remove unused drivers
+#$DriversToRemove | ForEach-Object { pnputil /remove-device $_.Driver /force }
